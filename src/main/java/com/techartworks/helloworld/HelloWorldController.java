baea65d5eb6569dbf27a28c7704a72152216fb9c
bhatnagarm <mitul_manik@yahoo.com>
2020-08-03 19:24:08 +1000

    feat(develop): add cat pictures.

--- src/main/java/com/techartworks/helloworld/HelloWorldController.java
+++ src/main/java/com/techartworks/helloworld/HelloWorldController.java
@@ -1,7 +1,16 @@
 package com.techartworks.helloworld;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class HelloWorldController {
@@ -9,6 +18,21 @@ public class HelloWorldController {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/random_image")
+    public ResponseEntity<byte[]> random_image()  throws IOException {
+        final OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        final Request request = new Request.Builder()
+                .url("https://source.unsplash.com/800x600/?cat") //TODO:artic-penguin
+                .method("GET", null)
+                .build();
+        final byte[] image = client.newCall(request).execute().body().bytes();
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentLength(image.length);
+        return new ResponseEntity<>(image, headers, HttpStatus.OK);
     }
 
 }
